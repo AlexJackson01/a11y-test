194c7a8fba80835c289b59f1e74061dc80234337
Alex Jackson <ajjackson22@gmail.com>
2025-08-17 19:43:57 +0100

    initial setup

---
+++ a11y-test/src/Images.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Image } from "react-native";
+export const Images = () => {
+  return (
+    <>
+      <Image />
+      <Image alt="image of" accessibilityLabel="Image of" />
+    </>
+  );
+};
